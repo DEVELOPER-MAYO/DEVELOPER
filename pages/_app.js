1d1af7d6107d6fa5ac15a0c3fbdf46a2b74f53bf
DEVELOPER-MAYO <maryorithsamaniego1@gmail.com>
2022-06-25 19:58:10 -0500

    Finished portafolio

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { DefaultSeo } from 'next-seo'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
 export default MyApp
